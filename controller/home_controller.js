b0600dd91928ac6db1502d6b1c2079a77f392203
connectrajnish <rajnishresonite@gmail.com>
2023-02-19 20:23:19 +0530

    Added feature to show the user who has posted, Populated using Mongoose

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -3,10 +3,19 @@
 const Post = require('../models/post');
 
 module.exports.home=function(req,res){                      //home, a name give to the function
-    Post.find({}, function(err, posts){
-        return res.render('home',{
-            title:'Home',
-            posts: posts
+    
+    //rendering POSTS without user i.e. without populating
+    // Post.find({}, function(err, posts){
+    //     return res.render('home',{
+    //         title:'Home',
+    //         posts: posts
+    //     });
+    // });
+    //rendering posts who has posted the post alongwith POST
+    Post.find({}).populate('user').exec(function(err, posts){
+            return res.render('home',{
+                title:'Home',
+                posts: posts
+            });
         });
-    });
 };
