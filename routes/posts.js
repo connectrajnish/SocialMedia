b0ffc389c9bb652d7e0fa8d5fcce7f624c3f56a0
connectrajnish <rajnishresonite@gmail.com>
2023-02-20 20:12:27 +0530

    Restricted posting without signing in

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const postsController=require('../controller/posts_controller');
 const router=express.Router();
+const passport = require('passport');
 
 //requesting for 'posts/posts' will return it back
 router.get('/posts',postsController.posts);
 
-router.post('/create',postsController.create);
+//to post something on the website after authentication
+router.post('/create',passport.checkAuthentication,postsController.create);
 
 module.exports=router;
 
